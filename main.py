76c7acd2bcd6e82950aa2b994ac94cb5a0a9a992
airvine98 <andrew.irvine@me.com>
2024-06-22 15:27:19 +0200

    default players

--- main.py
+++ main.py
@@ -23,48 +23,50 @@ negative_categories = ['Duffer', 'Worst Tackle', 'Drama Queen', 'Greedy Bastard'
 all_categories = positive_categories + negative_categories
 
 # Define organisers
-# Should dynamically update if an entry is submitted in the "other" field
+# TODO: Should be read in from the database
 organisers = ['Andrew', 'Alistair', 'Andy', 'Scott', 'Mark H', 'Mark B','Gary', 'Panu', 'Carlo', 'Simon']
 organisers.sort()
 organisers.append('Other')
+organisers = [''] + organisers
 
 # Define players
-# Should dynamically update if an entry is submitted in the "other" field
+# TODO: Should be read in from the database
 players = ['Andrew', 'Alistair', 'Andy', 'Scott', 'Mark H', 'Mark B','Gary', 'Panu', 'Carlo', 'Simon']
+players.sort()
+players.append('Other')
+players = [''] + players
 
 # Date input outside the form
 date = st.date_input('Date', datetime.now())
 
 # Voting organiser selection outside the form
-filled_by = st.selectbox('Voting Organiser', organisers, index=organisers.index('Andrew'))
+filled_by = st.selectbox('Voting Organiser', organisers, index=0)
 
 # Conditional input for 'Other' organiser
 if filled_by == 'Other':
     filled_by = st.text_input('Enter organiser name')
 
 
-with st.form('voting_form'):
-    
-    form_data = []
-    
-    for category in all_categories:
-        winner = st.selectbox(f'Winner of {category}', players)
-        if winner == 'Add New Player':
-            winner = st.text_input(f'Enter new player name for {category}')
-            players.add(winner)
-        
-        in_pub = st.checkbox(f'Was winner in the pub for {category}?')
+form_data = []
 
-        
-        if category in positive_categories:
-            points = 1
-        else:
-            points = -1
-        
-        if not in_pub:
-            points -= 1
-
-        
-        form_data.append([date, filled_by, category, winner, in_pub, points])
+for category in all_categories:
+    winner = st.selectbox(f'Winner of {category}', players, index = 0)
+    if winner == 'Add New Player':
+        winner = st.text_input(f'Enter new player name for {category}')
+        players.add(winner)
     
-    submitted = st.form_submit_button('Submit')
+    in_pub = st.checkbox(f'Was winner in the pub for {category}?')
+
+    
+    if category in positive_categories:
+        points = 1
+    else:
+        points = -1
+    
+    if not in_pub:
+        points -= 1
+
+    
+    form_data.append([date, filled_by, category, winner, in_pub, points])
+    
+submitted = st.button('Submit')
